4ce8fc438f89c0426649de783f1c2152ddaba2a7
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-03-17 23:56:06 +0100

    setting up mutation and queries :rocket:

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+const app = express();
+const PORT = 4000;
+const userData = require("./MOCK_DATA.json");
+import {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList} from "./graphql";
+const {graphqlHTTP} = require('express-graphql');
+
+const UserType = new GraphQLObject({
+    name : "User",
+    fields : () => ({
+        id : {type : GraphQLInt},
+        firstName : {type: GraphQLString},
+        lastName : {type: GraphQLString},
+        email : {type: GraphQLString},
+        password : {type: GraphQLString},
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name : "RootQueryType",
+    fields : {
+        getAllUsers : {
+            type: new GraphQLList(UserType),
+            args : { id : { type : GraphQLInt}}
+        },
+    }
+})
+
+const Mutation = "mutation"
+
+const schema = new GraphQLSchema({query :RootQuery , mutation: Mutation})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : true
+}))
+
+app.listen(PORT, ()=>{
+    console.log("SERVER RUNNING")
+})
