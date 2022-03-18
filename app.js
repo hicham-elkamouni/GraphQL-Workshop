ccb907e9b93c9f91bdf1ee156e609d67a383ad1c
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-03-18 17:57:45 +0100

    implementing mutation done :hammer:

--- app.js
+++ app.js
@@ -1,43 +1,75 @@
-import express from 'express';
+const express = require("express");
+const userData = require("./MOCK_DATA.json");
+// const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
 const app = express();
 const PORT = 4000;
-const userData = require("./MOCK_DATA.json");
-import {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList} from "./graphql";
-const {graphqlHTTP} = require('express-graphql');
 
-const UserType = new GraphQLObject({
-    name : "User",
-    fields : () => ({
-        id : {type : GraphQLInt},
-        firstName : {type: GraphQLString},
-        lastName : {type: GraphQLString},
-        email : {type: GraphQLString},
-        password : {type: GraphQLString},
-    })
-})
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    first_name: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
-    name : "RootQueryType",
-    fields : {
-        getAllUsers : {
-            type: new GraphQLList(UserType),
-            args : { id : { type : GraphQLInt}},
-            resolve(parent , args){
-                userData
-            }
-        },
-    }
-})
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return userData;
+      },
+    },
+  },
+});
 
-const Mutation = "mutation"
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
 
-const schema = new GraphQLSchema({query :RootQuery , mutation: Mutation})
+const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
-app.use('/graphql', graphqlHTTP({
+app.use("/graphql",graphqlHTTP({
     schema,
-    graphiql : true
-}))
+    graphiql: true,
+  })
+);
 
-app.listen(PORT, ()=>{
-    console.log("SERVER RUNNING")
-})
+app.listen(PORT, () => {
+  console.log("SERVER RUNNING");
+});
